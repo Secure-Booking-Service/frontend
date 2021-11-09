8b77c1bbffdbd9abef792acc2818da63bf9b24c7
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-09 16:35:25 +0000

    🐛 Fixed help screen

--- src/components/TerminalManager/index.ts
+++ src/components/TerminalManager/index.ts
@@ -6,7 +6,7 @@ import {
   echoCommand,
   loginCommand,
   manCommand,
-  registerCommand as registrationCommand
+  registerCommand as registrationCommand,
 } from "./commands";
 
 /**
@@ -140,8 +140,16 @@ export class TerminalManager {
       description: "Prints this help message.",
       callback: async (terminalMgr) => {
         terminalMgr.writeLine("All available commands:\r\n");
+        const width =
+          this.registeredCommands.reduce((longest, cmd) => {
+            return longest.command.length >= cmd.command.length ? longest : cmd;
+          }).command.length + 4;
         for (const cmd of this.registeredCommands) {
-          terminalMgr.writeLine(`${cmd.command}\t\t${cmd.description}`);
+          terminalMgr.writeLine(
+            cmd.command +
+              " ".repeat(width - cmd.command.length) +
+              cmd.description
+          );
         }
       },
     });
