c76359c9d544d04128a579837c1076a10d69f0d4
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-18 13:10:43 +0000

    ✏ Smaller fixes

--- src/components/TerminalManager/commands/booking/index.ts
+++ src/components/TerminalManager/commands/booking/index.ts
@@ -1,10 +1,11 @@
 import { ICommand } from "../..";
 import { executeSubCommand } from "../helper";
+import { abortCommand } from "./abort.command";
 import { checkCommand } from "./check.command";
 import { closeCommand } from "./close.command";
 import { creditcardCommand } from "./creditcard";
 import { newCommand } from "./new.command";
-import { passangerCommand } from "./passanger";
+import { passengerCommand } from "./passenger";
 import { selectCommand } from "./select.command";
 
 export const bookingCommand: ICommand = {
@@ -12,7 +13,7 @@ export const bookingCommand: ICommand = {
   description: "Manage the current booking",
   callback: async (manager, ...args) => {
     const usage = "Usage: booking OPERATION";
-    const registeredCommands = [ newCommand, passangerCommand, selectCommand, creditcardCommand, closeCommand, checkCommand ];
+    const registeredCommands = [ newCommand, passengerCommand, selectCommand, creditcardCommand, closeCommand, checkCommand, abortCommand];
     
     if (args.length === 0) {
       manager.writeError("Missing operation!");
