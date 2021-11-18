dd91ae056fc8158bfd8efae2fda0f8bad9b40443
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-18 13:08:59 +0000

    ⚡ Added hepler.ts

---
+++ src/components/TerminalManager/commands/booking/helpers.ts
@@ -0,0 +1,21 @@
+import { booking } from "@/overmind/booking";
+import { TerminalManager } from "../..";
+
+
+/**
+ * Checks if a new booking workflow has been started.
+ * If not it prints an error message and returns true
+ * 
+ * @export
+ * @param {TerminalManager} manager Terminal Manager instance
+ * @return {boolean} booking has not been started
+ */
+export function noCurrentBooking(manager: TerminalManager): boolean {
+  if (!booking.state.hasBookingStarted) {
+    manager.writeError("No booking found!")
+    manager.writeLine("Use 'booking new' to start a new booking!");
+    return true;
+  }
+
+  return false;
+}
