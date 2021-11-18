0e87034c0144e4b0bd98242587cabe84b204dd60
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-18 13:09:57 +0000

    ✨ Added new command

--- src/components/TerminalManager/commands/booking/new.command.ts
+++ src/components/TerminalManager/commands/booking/new.command.ts
@@ -1,9 +1,16 @@
 import { ICommand } from "../..";
-
+import { booking } from '@/overmind/booking';
 export const newCommand: ICommand = {
   command: "new",
   description: "Start a new booking process",
   callback: async (manager) => {
-    manager.write("booking new command");
+    booking.state.passengers
+    if (booking.state.hasBookingStarted) {
+      manager.writeError("A booking process has been already started!")
+      manager.writeLine("Use 'show' to view the details or 'abort' to cancel the booking");
+      return;
+    }
+    manager.writeLine("Start a new booking");
+    booking.actions.newBooking();
   },
 };
