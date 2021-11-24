07456e678630c870e414413639ace11b92af84c1
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-11-24 10:03:19 +0000

    🔥 Removed old code

--- src/components/TerminalManager/commands/booking/new.command.ts
+++ src/components/TerminalManager/commands/booking/new.command.ts
@@ -4,7 +4,6 @@ export const newCommand: ICommand = {
   command: "new",
   description: "Start a new booking process",
   callback: async (manager) => {
-    booking.state.passengers
     if (booking.state.hasBookingStarted) {
       manager.writeError("A booking process has been already started!")
       manager.writeLine("Use 'check' to view the details or 'abort' to cancel the booking");
