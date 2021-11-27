2077368ff96ad128d434e0902288030a41838ebc
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2021-11-27 21:45:36 +0100

    Apply suggestions from code review

--- src/components/TerminalManager/commands/flights/search.command.ts
+++ src/components/TerminalManager/commands/flights/search.command.ts
@@ -12,78 +12,78 @@ import { flights } from "@/overmind/flights";
 import { printFlightOffers } from "./helpers";
 
 export const searchCommand: ICommand = {
-    command: "search",
-    description: "Search a flight",
-    usage: ["ORIGIN-CODE", "DESTINATION-CODE", "DEPARTURE-DATE"],
-    callback: async (manager, ...args) => {
-        if (noCurrentBooking()) return;
-        let hasError = false;
+  command: "search",
+  description: "Search a flight",
+  usage: ["ORIGIN-CODE", "DESTINATION-CODE", "DEPARTURE-DATE"],
+  callback: async (manager, ...args) => {
+    if (noCurrentBooking()) return;
+    let hasError = false;
 
-        // Check if at least one passenger has been added
-        const adults = booking.state.passengers.length
-        if (adults === 0) {
-            manager.writeError("No passenger found!", true);
-            manager.writeLine("Please add at least one passenger using 'booking passenger add'")
-            hasError = true;
-        }
+    // Check if at least one passenger has been added
+    const adults = booking.state.passengers.length
+    if (adults === 0) {
+      manager.writeError("No passenger found!", true);
+      manager.writeLine("Please add at least one passenger using 'booking passenger add'")
+      hasError = true;
+    }
 
-        const [originLocationCode, destinationLocationCode, departureDate] = args;
+    const [originLocationCode, destinationLocationCode, departureDate] = args;
 
-        // Input validation
-        const length = { min: 3, max: 3 }
-        if (!isLength(originLocationCode, length) ||
-            !isLength(destinationLocationCode, length) ||
-            !isUppercase(originLocationCode) ||
-            !isUppercase(destinationLocationCode)) {
-            manager.writeError("Invalid origin or destination location code");
-            manager.writeLine("Please enter three uppercase characters");
-            manager.writeLine();
-            hasError = true;
-        }
-        if (!isDate(departureDate)) {
-            manager.writeError("Invalid date format: " + departureDate);
-            manager.writeLine("Please enter the departure date in YYYY-MM-DD format");
-            manager.writeLine();
-            hasError = true;
-        }
-        if (isDate(departureDate) && !isAfter(departureDate)) {
-            manager.writeError("Invalid date: " + departureDate);
-            manager.writeLine("Please enter a departure date in the future");
-            manager.writeLine();
-            hasError = true;
-        }
-        if (hasError) return;
+    // Input validation
+    const length = { min: 3, max: 3 }
+    if (!isLength(originLocationCode, length) ||
+      !isLength(destinationLocationCode, length) ||
+      !isUppercase(originLocationCode) ||
+      !isUppercase(destinationLocationCode)) {
+      manager.writeError("Invalid origin or destination location code");
+      manager.writeLine("Please enter three uppercase characters");
+      manager.writeLine();
+      hasError = true;
+    }
+    if (!isDate(departureDate)) {
+      manager.writeError("Invalid date format: " + departureDate);
+      manager.writeLine("Please enter the departure date in YYYY-MM-DD format");
+      manager.writeLine();
+      hasError = true;
+    }
+    if (isDate(departureDate) && !isAfter(departureDate)) {
+      manager.writeError("Invalid date: " + departureDate);
+      manager.writeLine("Please enter a departure date in the future");
+      manager.writeLine();
+      hasError = true;
+    }
+    if (hasError) return;
 
-        // Get, print and save flight offers
-        const requestConfig: AxiosRequestConfig = {
-            params: {
-                originLocationCode,
-                destinationLocationCode,
-                departureDate,
-                adults
-            }
-        }
-        try {
-            const apiResponse = await api.get("/flights", requestConfig)
-            if (apiResponse.status !== 200) {
-                printApiError(apiResponse);
-                return;
-            }
-            const flightOffers = apiResponse.data.data;
-            if (flightOffers.length > 0) {
-                manager.writeSuccess(`Found ${flightOffers.length} flights`, true);
-                printFlightOffers(flightOffers);
-            } else {
-                manager.writeError(`No flights found!`, true);
-            }
-            flights.actions.addFlightOffers(flightOffers);
-        }
-        catch (error: unknown) {
-            if (error !== undefined && error instanceof Error) {
-                manager.writeError(error.message);
-                manager.writeLine();
-                console.error(error);
-            }
-        }
-    },
+    // Get, print and save flight offers
+    const requestConfig: AxiosRequestConfig = {
+      params: {
+        originLocationCode,
+        destinationLocationCode,
+        departureDate,
+        adults
+      }
+    }
+    try {
+      const apiResponse = await api.get("/flights", requestConfig)
+      if (apiResponse.status !== 200) {
+        printApiError(apiResponse);
+        return;
+      }
+      const flightOffers = apiResponse.data.data;
+      if (flightOffers.length > 0) {
+        manager.writeSuccess(`Found ${flightOffers.length} flights`, true);
+        printFlightOffers(flightOffers);
+      } else {
+        manager.writeError(`No flights found!`, true);
+      }
+      flights.actions.addFlightOffers(flightOffers);
+    }
+    catch (error: unknown) {
+      if (error !== undefined && error instanceof Error) {
+        manager.writeError(error.message);
+        manager.writeLine();
+        console.error(error);
+      }
+    }
+  },
 };
