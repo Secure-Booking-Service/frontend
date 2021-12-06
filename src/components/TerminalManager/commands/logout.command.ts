1b1ff238fa8acbc6ecfd2052e2aaa64dd3dfbdf1
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-12-06 16:13:31 +0100

    ⚡ Improved logout command

--- src/components/TerminalManager/commands/logout.command.ts
+++ src/components/TerminalManager/commands/logout.command.ts
@@ -1,17 +1,30 @@
 import { user } from "@/overmind/user";
-import store from "@/store";
+import { api } from "@/components/utils/ApiUtil";
 import { ICommand } from "..";
+import { apiErrorHandler } from "../apierrorhandler";
+import { printApiError } from "./helper";
 
 export const logoutCommand: ICommand = {
   command: "logout",
+  loginRequired: true,
   description: "Ends your current user session",
   callback: async (manager) => {
     try {
-      await store.dispatch("logout");
-      user.actions.isLoggedOut();
-      manager.writeSuccess("Logged out successfully! Have a nice day!", true);
-    } catch {
-      manager.writeError("You are not logged in!");
+      // 1. Get options from server
+      const apiOptionsResponse = await api.get("/authentication/logout");
+      if (apiOptionsResponse.status !== 200) return printApiError(apiOptionsResponse);
+
+      // 2. Logged out successful!
+      user.actions.setIsLoggedIn();
+      return manager.writeSuccess("Logged out successfully! Have a nice day!", true);
+    } catch (error: unknown) {
+      if ((error as any)?.code !== undefined || (error as any)?.error !== undefined) 
+        return apiErrorHandler(manager, error as any);
+
+      // Request failed locally - maybe no internet connection etc?
+      return manager.writeError(
+        "Something went wrong locally - Check internet?"
+      );
     }
   },
 };
