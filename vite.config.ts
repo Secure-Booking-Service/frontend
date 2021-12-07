c537f3e0645b957634c3adaa084bba2501747bed
tjarbo <16938041+tjarbo@users.noreply.github.com>
2021-12-07 18:24:26 +0000

    ⚡ Removed vuex and installed vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import polyfillNode from 'rollup-plugin-polyfill-node';
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        polyfillNode({}),
+      ],
+    },
+  },
+})
