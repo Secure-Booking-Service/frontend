05f73325c0acb54450421bc1b8af39407950da54
Anton Plagemann <54081139+antonplagemann@users.noreply.github.com>
2021-12-12 12:26:23 +0100

    Fix wrong error handling

--- src/TerminalManager/commands/sudo/user/role/add.command.ts
+++ src/TerminalManager/commands/sudo/user/role/add.command.ts
@@ -3,8 +3,8 @@ import { Roles } from "@secure-booking-service/common-types/Roles";
 import { api } from "@/utils/ApiUtil";
 import isEmail from "validator/lib/isEmail";
 import isIn from "validator/lib/isIn";
-import { apiErrorHandler } from "@/TerminalManager/apierrorhandler";
 import { user } from "@/overmind/user";
+import { printApiError } from "@/TerminalManager/commands/helper";
 
 export const addCommand: ICommand = {
   command: "add",
@@ -43,7 +43,7 @@ export const addCommand: ICommand = {
 
       const apiReponse = await api.put('/user/' + email, payload);
       
-      if (apiReponse.status !== 200) throw apiErrorHandler(manager, apiReponse.data.error);
+      if (apiReponse.status !== 200) throw printApiError(apiReponse);
       
       manager.writeInfo("All changes will be applied at the next user login.", true);
       manager.writeSuccess("User updated successfully!", true);
